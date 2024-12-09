9bdcf1a2766969633b5e2d74e3dc9def029f0018
ahmed40keseli <krkkseli@gmail.com>
2024-12-09 13:51:03 +0300

    +

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const app = express();
 const User = require('./routes/auth.js');
 const Task = require('./routes/task.js'); 
 require('dotenv').config();
-// const sequelize = require('./config/database.js'); 
 const cors = require('cors');
 
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
